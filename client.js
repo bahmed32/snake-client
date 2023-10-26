d815e7334f2bf4140eb132f3263e6c21b4c1a41e
Bilqis Ahmed <bilqis.ali32@gmail.com>
2023-10-26 03:20:24 +0000

    Stop function incorported

--- client.js
+++ client.js
@@ -23,29 +23,8 @@ const connect = function() {
     conn.write("Name:ABA");
 
   });
-  conn.on("connect", () => {
-    conn.write("Move left");
-  });
-
-  // process.stdin.on("data", (data) => {
-  //   console.log("data", data);
-
-  // //   data = data.trim();
-
-  // //   if (data === 'a') {
-  // //     conn.write("Move: left");
-  // //   }
-  // //   if (data === 'w') {
-  // //     conn.write("Move: up");
-  // //   }
-  // //   if (data === 's') {
-  // //     conn.write("Move: down");
-  // //   }
-  // //   if (data === 'd') {
-  // //     conn.write("Move: right");
-  // //   }
-  // // }
-  // );
+ 
+  
 
   return conn;
 };
--- play.js
+++ play.js
@@ -1,30 +1,42 @@
 const net = require("net");
+const connnec = require("./client")
 
 // establishes a connection with the game server
-const connect = function() {
-  const conn = net.createConnection({
-    host: '165.227.47.243',
-      port: 50541,
-      
-  });
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  connection.on('data', (data) => {
-    console.log('server says:', data);
-  });
-  
-
-  conn.on("connect", ()  => {
-    console.log('Connection is successfully established')
-  })
-
-  return conn;
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  process.stdin.on("data", handleUserInput);
+  return stdin;
 };
 
+const handleUserInput = function(data){
+  if (data === '\u0003') {
+  process.exit();
+}
+};
 
+setupInput();
 // listen for incoming data
 
 // const connection = net.createConnection(connect());
 
+// data = data.trim();
+
+// if (data === 'a') {
+//   conn.write("Move: left");
+// }
+// if (data === 'w') {
+//   conn.write("Move: up");
+// }
+// if (data === 's') {
+//   conn.write("Move: down");
+// }
+// if (data === 'd') {
+//   conn.write("Move: right");
+// }
+
+// process.stdin.on("data", (data) => {
+//   console.log("data", data);
