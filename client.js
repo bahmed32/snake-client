bb3e58c8eb27229ffeffef0d8318603de1aeb80a
Bilqis Ahmed <bilqis.ali32@gmail.com>
2023-10-26 01:41:05 +0000

    Update snake 3

--- client.js
+++ client.js
@@ -4,11 +4,48 @@ const net = require("net");
 const connect = function() {
   const conn = net.createConnection({
     host: '165.227.47.243',
-      port: 50541,
+    port: 50541,
   });
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
+
+  conn.on('data', (data) => {
+    console.log('server says:', data);
+  });
+
+
+  conn.on("connect", () => {
+    console.log('Connection is successfully established');
+  });
+
+  conn.on("connect", () => {
+    conn.write("Name:ABA");
+
+  });
+  conn.on("connect", () => {
+    conn.write("Move left");
+  });
+
+  // process.stdin.on("data", (data) => {
+  //   console.log("data", data);
+
+  // //   data = data.trim();
+
+  // //   if (data === 'a') {
+  // //     conn.write("Move: left");
+  // //   }
+  // //   if (data === 'w') {
+  // //     conn.write("Move: up");
+  // //   }
+  // //   if (data === 's') {
+  // //     conn.write("Move: down");
+  // //   }
+  // //   if (data === 'd') {
+  // //     conn.write("Move: right");
+  // //   }
+  // // }
+  // );
 
   return conn;
 };
@@ -16,6 +53,8 @@ const connect = function() {
 
 
 // module.exports = {connect}
+connect();
 
 module.exports = {
-connect,}
+  connect
+};
--- play.js
+++ play.js
@@ -1,28 +1,30 @@
-// const net = require("net");
+const net = require("net");
 
-// // establishes a connection with the game server
-// const connect = function() {
-//   const conn = net.createConnection({
-//     host: '165.227.47.243',
-//       port: 50541,
-//   });
+// establishes a connection with the game server
+const connect = function() {
+  const conn = net.createConnection({
+    host: '165.227.47.243',
+      port: 50541,
+      
+  });
 
-//   // interpret incoming data as text
-//   conn.setEncoding("utf8");
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
 
-//   return conn;
-// };
+  connection.on('data', (data) => {
+    console.log('server says:', data);
+  });
+  
 
-const {connect} = require("./client.js")
+  conn.on("connect", ()  => {
+    console.log('Connection is successfully established')
+  })
+
+  return conn;
+};
+
+
 // listen for incoming data
 
 // const connection = net.createConnection(connect());
-connection = connect();
 
-connection.on('data', (data) => {
-  console.log('server says:', data);
-});
-
-
-console.log("Connecting ...");
-connect()
